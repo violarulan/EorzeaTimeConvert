88dd958f3969a4702e2805478865472b54a588d9
Viola Rulan <ybai38@asu.edu>
2017-03-13 22:13:57 +0900

    initial

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # EorzeaTimeConvert
+
 Convert Earth time to FFXIV Eorzea time
+
+## Time
+
+The measurement and expression of time in Eorzea, like the real world, is governed by a fundamental set of rules. The Dusty Tomes found in various guilds explain these standards in a book called The Five Ages - An Eorzean Chronology, albeit in heavily archaic language. In-game, time can be expressed in either a 24-hour clock format, or a 12-hour format with a.m./p.m. You can also change the clock to be Eorzea Time (ET), System Time (ST) of the server you're on, or Local Time (LT) showing your current real-life time. There is also an icon showing the current weather for the zone you're in, in the upper-right corner of the mini-map.
+
+## Eorzean Time Units and Conversions
+
+ Eorzean Increments | Components in Eorzean Units |  Conversion into Earth Time
+--------------------|-----------------------------|----------------------------
+       1 minute     |          60 seconds         |        2 11/12 seconds
+        1 bell      |          60 minutes         |     2 minutes, 55 seconds
+        1 sun       |           24 bells          |          70 minutes
+        1 week      |            8 suns           |      9 hours, 20 minutes
+        1 moon      |           32 suns           |      37 hours, 20 minutes
+        1 year      |           12 moons          |        18 days, 16 hours 
+
+## Usage
+
+    package main
+
+    import (
+        "fmt"
+        "time"
+        c "github.com/violarulan/EorzeaTimeConvert"
+    )
+
+    func main(){
+        // Convert a time.Time to a EorzeaTime
+        var ret = c.ConvertToEorzeaTime(time.Now())
+        fmt.Println(ret)
+        // {924 6 30 2 31 53}
+
+        var str = c.ConvertToEorzeaTimeString(time.Now(), "%s-%s-%s %s:%s:%s")
+        fmt.Println(str)
+        // 924-06-30 08:01:22
+    }
+
+## Contribute
+
+Please feel free to submit issues and PRs.
---
+++ convert.go
@@ -0,0 +1,71 @@
+package EorzeaTimeConvert
+
+import (
+        "fmt"
+        "math"
+        "strconv"
+        "time"
+)
+
+const (
+        YEAR   = 33177600
+        MONTH  = 2764800
+        DAY    = 86400
+        HOUR   = 3600
+        MINUTE = 60
+        SECOND = 1
+)
+
+type EorzeaTime struct {
+        yearVal   int64
+        monthVal  int64
+        dayVal    int64
+        hourVal   int64
+        minuteVal int64
+        secondVal int64
+}
+
+var (
+        EORZEA_TIME_CONSTANT float64 = float64(3600) / float64(175)
+)
+
+func ConvertToEorzeaTime(time time.Time) EorzeaTime {
+        earthTime := float64(time.Unix())
+        eorzeaTime := int64(math.Floor(earthTime * EORZEA_TIME_CONSTANT))
+
+        var ret EorzeaTime
+        ret.yearVal = int64(math.Floor(float64(eorzeaTime / YEAR))) + 1
+        ret.monthVal = int64(math.Floor(float64(eorzeaTime / MONTH % 12))) + 1
+        ret.dayVal = int64(math.Floor(float64(eorzeaTime / DAY % 32))) + 1
+        ret.hourVal = int64(math.Floor(float64(eorzeaTime / HOUR % 24)))
+        ret.minuteVal = int64(math.Floor(float64(eorzeaTime / MINUTE % 60)))
+        ret.secondVal = int64(math.Floor(float64(eorzeaTime / SECOND % 60)))
+
+        return ret
+}
+
+func ConvertToEorzeaTimeString(time time.Time, format string) string {
+
+        earthTime := float64(time.Unix())
+        eorzeaTime := int64(math.Floor(earthTime * EORZEA_TIME_CONSTANT))
+
+        yearVal := "" + strconv.FormatInt(int64(math.Floor(float64(eorzeaTime / YEAR))) + 1, 10)
+        monthVal := formatZero(strconv.FormatInt(int64(math.Floor(float64(eorzeaTime / MONTH%12))) + 1, 10))
+        dayVal := formatZero(strconv.FormatInt(int64(math.Floor(float64(eorzeaTime / DAY % 32 ))) + 1, 10))
+        hourVal := formatZero(strconv.FormatInt(int64(math.Floor(float64(eorzeaTime / HOUR % 24))), 10))
+        minuteVal := formatZero(strconv.FormatInt(int64(math.Floor(float64(eorzeaTime / MINUTE % 60))), 10))
+        secondVal := formatZero(strconv.FormatInt(int64(math.Floor(float64(eorzeaTime / SECOND % 60))), 10))
+
+        var ret string
+        ret = fmt.Sprintf(format, yearVal, monthVal, dayVal, hourVal, minuteVal, secondVal)
+        return ret
+}
+
+func formatZero(str string) string {
+        if len(str) == 1 {
+                return "0" + str
+        } else {
+                return str
+        }
+}
+
